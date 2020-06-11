720a08e2c97669317c08efa29283cad50e4d1617
amazingcloudops <debasis.parida@outlook.com>
2020-06-11 07:32:39 +0530

    added variable example

---
+++ basics/basic_variable.py
@@ -0,0 +1,10 @@
+name = "Debasis"
+age, house_number = 35, 117
+print (name + " age is " + str(age) + "and he stays at house number " + str(house_number))
+
+# Below is the example to show the ID for the variable and the value are same in Python
+
+house_number1 = 117
+print(id (house_number))
+print(id (house_number1))
+print(id (117))
