572e4b131c28dcf358afe53dfabe2c2dd8b2da41
amazingcloudops <debasis.parida@outlook.com>
2020-06-12 15:57:54 +0530

    added string_index.py

---
+++ data_types/string_index.py
@@ -0,0 +1,9 @@
+name = "John Citizen"
+#Print first character using index
+print (name[0])
+
+#Print last character using negative index
+print (name[-1])
+
+#Print the length of the string using len()
+print(len(name))
