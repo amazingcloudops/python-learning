760b74e74b54d8881215d2353144e5bc2a91e4b4
amazingcloudops <debasis.parida@outlook.com>
2020-06-12 16:50:21 +0530

    added string operators and formatting

---
+++ data_types/string_operators_formatting.py
@@ -0,0 +1,13 @@
+#String Concatenation and repetation
+str1 = "Amazon"
+str2 = "AWS"
+
+print (str1 + " " + str2)
+print (str1 * 5)
+
+#Check if a character exists or not (BOOLEAN Output)
+print ("A" in str1)
+print ("x" in str1)
+print ("z" not in str2)
+
+
