7f8c2d73c5070372b1efe983b85d077ad34b1782
amazingcloudops <debasis.parida@outlook.com>
2020-06-13 17:15:12 +0530

    list file updated

---
+++ lists/basic_list.py
@@ -0,0 +1,21 @@
+#Basic List Example
+
+list1 = [10, 20, 30]
+print(type(list1))
+print(list1)
+
+list2 = ["John", 14, 30]
+print(list2)
+
+#Nested List
+
+list3 = ["John", [10, 12, 14], 90]
+print(list3)
+
+#List Index
+print(list1[1])
+print(list3[1][0])
+
+
+print(len(list1))
+print(len(list3))
