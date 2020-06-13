47a94ac11191d28519491d7ed018038b6915c912
amazingcloudops <debasis.parida@outlook.com>
2020-06-13 12:39:59 +0530

    f-string and slicing added

---
+++ data_types/f-string.py
@@ -0,0 +1,11 @@
+#F-String Example
+
+name = "John"
+age = 26
+salary = 8.7
+
+print(f"Employee Name: {name}, Employee Age: {age}, Employee Salary: {salary}")
+
+print(f"Employee Name: {name}, Employee Age: {age + 5}, Employee Salary: {salary}")
+
+print(f"Employee Name: {name.upper()}, Employee Age: {age + 5}, Employee Salary: {salary}")
---
+++ data_types/string_slicing.py
@@ -0,0 +1,17 @@
+#String Slicing
+
+name = "John Citizen"
+
+print (name[0:])
+print (name[:7])
+
+print (name[0: 4])
+
+print (name[:])
+
+#Negative Index using slicing
+
+print (name[-1])
+print (name[-7: -1])
+print (name[-7:])
+
