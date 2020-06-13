635ad8cd789d3fa2257818e2b856e8b65095a244
amazingcloudops <debasis.parida@outlook.com>
2020-06-13 16:32:19 +0530

    numbers added

---
+++ numbers/math_operators.py
@@ -0,0 +1,15 @@
+#Math Operator Pecedence
+#first power operations, then multiplication/division/modulo, and finally addition/subscration
+a = 10
+b = 3
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a // b)
+print (a % b)
+print (a > b)
+print (a < b)
+print (a <= b)
+print (a == b)
+print (a != b)
